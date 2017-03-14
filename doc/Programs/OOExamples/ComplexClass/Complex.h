90e586f1936ac14fe076bc32fad34c5d94a98079
mhjensen <morten.hjorth-jensen@fys.uio.no>
2017-03-14 18:17:12 +0100

    Added programs and update on oo part

--- doc/Programs/OOExamples/ComplexClass/Complex.h
+++ doc/Programs/OOExamples/ComplexClass/Complex.h
@@ -12,16 +12,15 @@ private:
 public:
   Complex ();                              // Complex c;
   Complex (double re = 0.0, double im = 0.0); // Definition of a complex variable;
-  Complex (const Complex& c);              // Usage: Complex c(a);   // equate two complex variables
-  Complex& operator= (const Complex& c); // c = a;   //  equate two complex variables, same as previous
+  Complex& operator= (const Complex& c); // c = a;   //  equate two complex variables
   ~Complex () {}                        // destructor
   double   Re () const;        // double real_part = a.Re();
   double   Im () const;        // double imag_part = a.Im();
   double   abs () const;       // double m = a.abs(); // modulus
-  friend Complex operator+ (const Complex&  a, const Complex& b);
-  friend Complex operator- (const Complex&  a, const Complex& b);
-  friend Complex operator* (const Complex&  a, const Complex& b);
-  friend Complex operator/ (const Complex&  a, const Complex& b);
+  friend Complex operator+ (const Complex& a, const Complex& b);
+  friend Complex  operator- (const Complex& a, const Complex& b);
+  friend Complex operator* (const Complex& a, const Complex& b);
+  friend Complex operator/ (const Complex& a, const Complex& b);
 };
 
 #endif
--- doc/Programs/OOExamples/ComplexClass/complex.cpp
+++ doc/Programs/OOExamples/ComplexClass/complex.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
 #include <cmath>
-#include <complex>
-using namespace std;
-int main()
+#include "Complex.h"
+
+Complex:: Complex () { re = im = 0.0; }
+
+Complex:: Complex (double re_a, double im_a) {re = re_a; im = im_a; }
+
+double Complex:: Re () const { return re; } //  getting the real part
+double Complex:: Im () const { return im; }  //   and the imaginary part
+double Complex:: abs () const { return sqrt(re*re + im*im); }
+Complex& Complex:: operator= (const Complex& c)
 {
-  std::complex<double> x(6.1,8.2), y(0.5,1.3);
-  // write out x+y
-  cout << x + y << x*y  << endl;
-  return 0;
+  re = c.re;
+  im = c.im;
+  return *this;
 }
+Complex operator+ (const Complex& a, const Complex& b) { return Complex (a.re + b.re, a.im + b.im); }
+Complex operator- (const Complex& a, const Complex& b) { return Complex (a.re - b.re, a.im - b.im); }
+Complex operator* (const Complex& a, const Complex& b) {return Complex(a.re*b.re - a.im*b.im, a.im*b.re + a.re*b.im);}
+Complex operator/ (const Complex& a, const Complex& b) {return Complex( (a.re*b.re +a.im*b.im)/(b.re*b.re+b.im*b.im), (-a.re*b.im + a.im*b.re)/(b.re*b.re+b.im*b.im));}
