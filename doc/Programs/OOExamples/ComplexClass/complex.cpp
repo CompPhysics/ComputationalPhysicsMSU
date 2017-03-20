382c8aa8312aba9d2b24241fc0c54dba9f7c9435
mhjensen <morten.hjorth-jensen@fys.uio.no>
2017-03-20 19:54:44 -0400

    Added program on autocorrelation function

--- doc/Programs/OOExamples/ComplexClass/complex.cpp
+++ doc/Programs/OOExamples/ComplexClass/complex.cpp
@@ -1,13 +1,15 @@
 #include <cmath>
 #include "Complex.h"
 
-Complex:: Complex () { re = im = 0.0; }
+template<class T>
+Complex<T>:: Complex () { re = im = 0.0; }
 
-Complex:: Complex (double re_a, double im_a) {re = re_a; im = im_a; }
+template<class T>
+Complex<T>:: Complex (T re_a, T im_a) {re = re_a; im = im_a; }
 
-double Complex:: Re () const { return re; } //  getting the real part
-double Complex:: Im () const { return im; }  //   and the imaginary part
-double Complex:: abs () const { return sqrt(re*re + im*im); }
+template<class T> Complex<T>:: Re () const { return re; } //  getting the real part
+template<class T> Complex<T>:: Im () const { return im; }  //   and the imaginary part
+template<class T> Complex<T>:: abs () const { return sqrt(re*re + im*im); }
 Complex& Complex:: operator= (const Complex& c)
 {
   re = c.re;
