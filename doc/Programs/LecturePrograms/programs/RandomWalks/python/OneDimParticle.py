a7068b9cfe3d7c4b0d781ce8f45a77de4a6849ee
mhjensen <morten.hjorth-jensen@fys.uio.no>
2018-04-12 12:41:49 -0400

    updated the random walk slides

--- doc/Programs/LecturePrograms/programs/RandomWalks/python/OneDimParticle.py
+++ doc/Programs/LecturePrograms/programs/RandomWalks/python/OneDimParticle.py
@@ -5,20 +5,21 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import random
-from math import sqrt, exp
+from math import sqrt, exp, log
 # initialize the rng with a seed
 random.seed()
 # Hard coding of input parameters
 MCcycles = 100000
 Temperature = 2.0
 beta = 1./Temperature
-InitialVelocity = 0.0
+InitialVelocity = -2.0
 CurrentVelocity = InitialVelocity
 Energy = 0.5*InitialVelocity*InitialVelocity
 VelocityRange = 10*sqrt(Temperature)
-VelocityStep = 2*VelocityRange/100.
+VelocityStep = 2*VelocityRange/10.
 AverageEnergy = Energy
 AverageEnergy2 = Energy*Energy
+VelocityValues = np.zeros(MCcycles)
 # The Monte Carlo sampling with Metropolis starts here
 for i in range (1, MCcycles, 1):
     TrialVelocity = CurrentVelocity + (2.0*random.random() - 1.0)*VelocityStep
@@ -26,6 +27,7 @@ for i in range (1, MCcycles, 1):
     if random.random() <= exp(-beta*EnergyChange):
         CurrentVelocity = TrialVelocity
         Energy += EnergyChange
+        VelocityValues[i] = CurrentVelocity
     AverageEnergy += Energy
     AverageEnergy2 += Energy*Energy
 #Final averages
@@ -33,3 +35,11 @@ AverageEnergy = AverageEnergy/MCcycles
 AverageEnergy2 = AverageEnergy2/MCcycles
 Variance = AverageEnergy2 - AverageEnergy*AverageEnergy
 print(AverageEnergy, Variance)
+n, bins, patches = plt.hist(VelocityValues, 400, facecolor='green')
+
+plt.xlabel('$v$')
+plt.ylabel('Velocity distribution P(v)')
+plt.title(r'Velocity histogram at $k_BT=2$')
+plt.axis([-5, 5, 0, 600])
+plt.grid(True)
+plt.show()
